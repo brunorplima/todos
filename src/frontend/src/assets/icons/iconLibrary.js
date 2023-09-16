dc4a02846f08dfb2159724c6689b79e4f8847c7b
Bruno Lima <brunolima.cad@gmail.com>
2023-09-15 22:57:17 -0600

    added icons to the library

--- src/frontend/src/assets/icons/iconLibrary.js
+++ src/frontend/src/assets/icons/iconLibrary.js
@@ -3,11 +3,15 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons/faPen";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons/faChevronDown";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons/faChevronUp";
+import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons/faFloppyDisk";
+import { faXmark } from "@fortawesome/free-solid-svg-icons/faXmark";
 
 const icons = [
   faPen,
   faChevronUp,
-  faChevronDown
+  faChevronDown,
+  faFloppyDisk,
+  faXmark
 ]
 
 const startIconLibrary = () => {
