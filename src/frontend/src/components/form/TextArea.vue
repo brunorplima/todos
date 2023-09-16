e02f18e84b9805221d48f8d3a74206fb59db8cc0
Bruno Lima <brunolima.cad@gmail.com>
2023-09-15 22:58:07 -0600

    created input components

---
+++ src/frontend/src/components/form/TextArea.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from "vue";
+
+const { id, label, value } = defineProps([
+  'id',
+  'label',
+  'value'
+])
+const emit = defineEmits(['changeValue'])
+
+const inputValue = ref(value)
+
+function changeValue() {
+  emit('changeValue', inputValue)
+}
+</script>
+
+<template>
+  <div class="form-group">
+    <label :for="id" class="custom-label">{{ label }}</label>
+    <textarea :id="id" v-model="inputValue" class="form-control" @change="changeValue"></textarea>
+  </div>
+</template>
