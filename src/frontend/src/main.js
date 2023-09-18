c1b524ea54796986a68a8c1c5114987df070ff19
Bruno Lima <brunolima.cad@gmail.com>
2023-09-18 00:06:42 -0600

    vue router install and setup

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -3,9 +3,11 @@ import './style.css'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import startIconLibrary from "./assets/icons/iconLibrary.js";
+import router from "./router.js";
 
 const { componentName, FontAwesomeIcon } = startIconLibrary()
 
 createApp(App)
   .component(componentName, FontAwesomeIcon)
+  .use(router)
   .mount('#app')
