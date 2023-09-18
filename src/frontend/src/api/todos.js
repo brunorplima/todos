606512ccf56e8fe9b84ebd65f2b9d3a2f9de5997
Bruno Lima <brunolima.cad@gmail.com>
2023-09-18 00:09:42 -0600

    created todo post api request function

--- src/frontend/src/api/todos.js
+++ src/frontend/src/api/todos.js
@@ -2,4 +2,5 @@ import client from "./client.js";
 import { getTodosUrl } from "./urls.js";
 
 export const getAllTodos = () => client.get(getTodosUrl())
+export const createTodo = (todo) => client.post(getTodosUrl(), todo)
 export const updateTodo = (todo) => client.put(getTodosUrl(todo.id), todo)
