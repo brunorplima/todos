32a0cf92af11e6cd593ce6ce84dd64a11899cb09
Bruno Lima <brunolima.cad@gmail.com>
2023-09-21 00:38:28 -0600

    split todo list in 2 different arrays

--- src/frontend/src/components/TodoList.vue
+++ src/frontend/src/components/TodoList.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { getAllTodos } from "../api/todos.js";
 import TodoCard from "./TodoCard.vue";
 
 const todos = ref([])
+const showCompleted = ref(true)
+
+const completedTodos = computed(() => todos.value.filter(t => t.completed))
+const notCompletedTodos = computed(() => todos.value.filter(t => !t.completed))
 
 onMounted(() => {
   const fetchData = async () => todos.value = await getAllTodos()
@@ -24,9 +28,9 @@ function applyDeleteChangesToTodos(id) {
 
 <template>
   <div id="todos" class="d-flex flex-column align-items-sm-center">
-    <ul id="todo-list" class="list-unstyled w-100">
+    <ul id="todo-list" class="list-unstyled w-100 mb-5">
       <li
-        v-for="(todo, index) in todos"
+        v-for="(todo, index) in notCompletedTodos"
         :key="todo.id"
         class="card w-100"
         :class="index !== todos.length - 1 && 'mb-3'"
@@ -38,6 +42,32 @@ function applyDeleteChangesToTodos(id) {
         />
       </li>
     </ul>
+
+    <div v-if="completedTodos.length">
+      <div class="d-flex justify-content-between">
+        <h5>Completed</h5>
+        <div
+          class="text-muted btn btn-link"
+          @click="showCompleted = !showCompleted"
+        >
+          {{ showCompleted ? 'hide' : 'show' }}
+        </div>
+      </div>
+      <ul v-if="showCompleted" id="todo-list" class="list-unstyled w-100">
+        <li
+          v-for="(todo, index) in completedTodos"
+          :key="todo.id"
+          class="card w-100"
+          :class="index !== todos.length - 1 && 'mb-3'"
+        >
+          <TodoCard
+            :todo="todo"
+            @updateTodo="applyUpdateChangesToTodo"
+            @deleteTodo="applyDeleteChangesToTodos"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
